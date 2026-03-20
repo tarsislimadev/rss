76ee36f802829b660a919b159a149fea474c33b1
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-20 19:21:37 -0300

    Fix main file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('RSS feed')
+// npm install --save rss-parser
+
+const readme_url = 'https://raw.githubusercontent.com/tarsislimadev/rss/refs/heads/main/README.md'
+
+const Parser = require('rss-parser')
+
+const parseISOString = (date = (new Date).toISOString()) => date.split('T').at(0)
+
+const getReadMeText = () => fetch(readme_url).then((res) => res.text())
+
+const parseRssItem = (item) => console.log('RSS feed', item.title, parseISOString(item.isoDate))
+
+const parseRssFeed = (url) => (new Parser()).parseURL(url)
+  .then((res) => res.items.map((item) => parseRssItem(item)))
+
+const state = { urls: null }
+
+getReadMeText()
+  .then((text) => state.urls = text.split('\n').slice(1).filter(Boolean))
+  .then(() => state.urls.map((url) => parseRssFeed(url)))
+  .catch((err) => console.error(err))
